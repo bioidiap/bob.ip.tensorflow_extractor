b83dc957af69dc67ca65f3c5668b43279672e262
Amir MOHAMMADI <amir.mohammadi@idiap.ch>
2020-03-11 17:11:43 +0100

    Tensorflow 2 compatibility

--- bob/ip/tensorflow_extractor/test.py
+++ bob/ip/tensorflow_extractor/test.py
@@ -1,46 +1,10 @@
 import bob.io.base
 import bob.io.image
 from bob.io.base.test_utils import datafile
-import bob.ip.tensorflow_extractor
-import tensorflow as tf
-
-import pkg_resources
 import numpy
 import json
-import os
 
 numpy.random.seed(10)
-
-slim = tf.contrib.slim
-from . import scratch_network
-
-
-def test_output():
-
-    # Loading MNIST model
-    filename = os.path.join(
-        pkg_resources.resource_filename(__name__, "data"), "model.ckp"
-    )
-    inputs = tf.placeholder(tf.float32, shape=(None, 28, 28, 1))
-
-    # Testing the last output
-    graph = scratch_network(inputs)
-    extractor = bob.ip.tensorflow_extractor.Extractor(filename, inputs, graph)
-
-    data = numpy.random.rand(2, 28, 28, 1).astype("float32")
-    output = extractor(data)
-    assert extractor(data).shape == (2, 10)
-    del extractor
-
-    # Testing flatten
-    inputs = tf.placeholder(tf.float32, shape=(None, 28, 28, 1))
-    graph = scratch_network(inputs, end_point="flatten1")
-    extractor = bob.ip.tensorflow_extractor.Extractor(filename, inputs, graph)
-
-    data = numpy.random.rand(2, 28, 28, 1).astype("float32")
-    output = extractor(data)
-    assert output.shape == (2, 1690)
-    del extractor
 
 
 def test_facenet():
@@ -50,24 +14,6 @@ def test_facenet():
     data = numpy.random.rand(3, 160, 160).astype("uint8")
     output = extractor(data)
     assert output.size == 128, output.shape
-
-
-def test_drgan():
-    from bob.ip.tensorflow_extractor import DrGanMSUExtractor
-
-    extractor = DrGanMSUExtractor()
-    data = numpy.random.rand(3, 96, 96).astype("uint8")
-    output = extractor(data)
-    assert output.size == 320, output.shape
-
-
-def test_vgg16():
-    pass
-    # from bob.ip.tensorflow_extractor import VGGFace
-    # extractor = VGGFace()
-    # data = numpy.random.rand(3, 224, 224).astype("uint8")
-    # output = extractor(data)
-    # assert output.size == 4096, output.shape
 
 
 def test_mtcnn():
--- setup.py
+++ setup.py
@@ -34,42 +34,38 @@
 # administrative interventions.
 
 from setuptools import setup, dist
-dist.Distribution(dict(setup_requires=['bob.extension']))
+
+dist.Distribution(dict(setup_requires=["bob.extension"]))
 
 from bob.extension.utils import load_requirements, find_packages
+
 install_requires = load_requirements()
 
 # The only thing we do in this file is to call the setup() function with all
 # parameters that define our package.
 setup(
-
     # This is the basic information about your project. Modify all this
     # information before releasing code publicly.
-    name = 'bob.ip.tensorflow_extractor',
-    version = open("version.txt").read().rstrip(),
-    description = 'Feature extractor using tensorflow CNNs',
-
-    url = 'https://gitlab.idiap.ch/tiago.pereira/bob.ip.caffe_extractor',
-    license = 'BSD',
-    author = 'Tiago de Freitas Pereira',
-    author_email = 'tiago.pereira@idiap.ch',
-    keywords = 'bob, biometric recognition, evaluation',
-
+    name="bob.ip.tensorflow_extractor",
+    version=open("version.txt").read().rstrip(),
+    description="Feature extractor using tensorflow CNNs",
+    url="https://gitlab.idiap.ch/tiago.pereira/bob.ip.caffe_extractor",
+    license="BSD",
+    author="Tiago de Freitas Pereira",
+    author_email="tiago.pereira@idiap.ch",
+    keywords="bob, biometric recognition, evaluation",
     # If you have a better, long description of your package, place it on the
     # 'doc' directory and then hook it here
-    long_description = open('README.rst').read(),
-
+    long_description=open("README.rst").read(),
     # This line is required for any distutils based packaging.
-    packages = find_packages(),
-    include_package_data = True,
-
+    packages=find_packages(),
+    include_package_data=True,
     # This line defines which packages should be installed when you "install"
     # this package. All packages that are mentioned here, but are not installed
     # on the current system will be installed locally and only visible to the
     # scripts of this package. Don't worry - You won't need administrative
     # privileges when using buildout.
-    install_requires = install_requires,
-
+    install_requires=install_requires,
     # Your project should be called something like 'bob.<foo>' or
     # 'bob.<foo>.<bar>'. To implement this correctly and still get all your
     # packages to be imported w/o problems, you need to implement namespaces
@@ -80,8 +76,6 @@ setup(
     # Our database packages are good examples of namespace implementations
     # using several layers. You can check them out here:
     # https://github.com/idiap/bob/wiki/Satellite-Packages
-
-
     # This entry defines which scripts you will have inside the 'bin' directory
     # once you install the package (or run 'bin/buildout'). The order of each
     # entry under 'console_scripts' is like this:
@@ -93,17 +87,16 @@ setup(
     # installed under 'example/foo.py' that contains a function which
     # implements the 'main()' function of particular script you want to have
     # should be referred as 'example.foo:main'.
-
     # Classifiers are important if you plan to distribute this package through
     # PyPI. You can find the complete list of classifiers that are valid and
     # useful here (http://pypi.python.org/pypi?%3Aaction=list_classifiers).
-    classifiers = [
-      'Framework :: Bob',
-      'Development Status :: 4 - Beta',
-      'Intended Audience :: Developers',
-      'License :: OSI Approved :: BSD License',
-      'Natural Language :: English',
-      'Programming Language :: Python',
-      'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    classifiers=[
+        "Framework :: Bob",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
 )
